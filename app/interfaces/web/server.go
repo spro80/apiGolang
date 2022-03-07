9f6e64ed0f91dea6fa2f8f7f2d21c32620e9594a
David Diaz <daviddiaz@M-C02FH1TDMD6P.local>
2022-03-07 12:30:20 -0300

    add main.go and web-server

--- app/interfaces/web/server.go
+++ app/interfaces/web/server.go
@@ -2,23 +2,52 @@ package web
 
 import (
 	"fmt"
-	"log"
 	"net/http"
+	"time"
 
-	"github.com/gorilla/mux"
-	"github.com/rs/cors"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/labstack/gommon/log"
+	"github.com/spro80/apiGolang/app/infraestructure/controllers/order"
+	"github.com/spro80/apiGolang/app/interfaces/web/routes"
 )
 
-func HealtCheck() {
-	fmt.Println("Prueba Healtcheck")
+// "github.com/spro80/apiGolang/app/infraestructure/controllers/useCaseOne"
+
+var echoServer *echo.Echo
+
+func NewWebServer() {
+	echoServer = echo.New()
+	echoServer.HideBanner = true
+	//echoServer.Use(middleware.Recover())
+	echoServer.Use(middleware.CORS())
+	echoServer.Use(middleware.RequestID())
+	//echoServer.Validator = json_validator.NewJsonValidator()
+}
+
+func InitRoutes(saveOrderUseCase order.OrderControllerInterface) {
+	routes.NewHealthHandler(echoServer)
+	routes.NewSaveOrderHandler(echoServer, saveOrderUseCase)
 }
 
 func Start(port string) {
+	server := &http.Server{
+		Addr:         fmt.Sprintf(":%s", port),
+		ReadTimeout:  3 * time.Minute,
+		WriteTimeout: 3 * time.Minute,
+	}
+	log.Info("App listen in port %s", port)
+	echoServer.Logger.Fatal(echoServer.StartServer(server))
+}
+
+/*
+func HealtCheck() {
+	fmt.Println("Prueba Healtcheck")
+}
+func Start(port string) {
 	fmt.Println("Init in Start")
 	router := mux.NewRouter()
-
 	//router.HandleFunc("/health", HealtCheck()).Methods("GET")
-
 	PORT := port //os.Getenv("PORT")
 	if PORT == "" {
 		PORT = "9090"
@@ -26,3 +55,4 @@ func Start(port string) {
 	handler := cors.AllowAll().Handler(router)
 	log.Fatal(http.ListenAndServe(":"+PORT, handler))
 }
+*/
--- app/main.go
+++ app/main.go
@@ -3,16 +3,20 @@ package main
 import (
 	"fmt"
 
+	"github.com/spro80/apiGolang/app/infraestructure/controllers/order"
 	"github.com/spro80/apiGolang/app/interfaces/web"
 )
 
 func main() {
 	fmt.Println("Init in main")
 
+	//Controller
+	orderController := order.NewOrderController()
+
 	//WebServer
 	var port string = "8080"
 	web.NewWebServer()
-	web.InitRoutes()
+	web.InitRoutes(orderController)
 	web.Start(port)
 	//web.Start(config.GetString("web.port"))
 }
