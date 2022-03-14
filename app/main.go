7d6e0941e618a3264cb811cdd9599e8c86d0f448
David Diaz <daviddiaz@M-C02FH1TDMD6P.local>
2022-03-13 21:36:27 -0300

    modify main, add called to use case

--- app/main.go
+++ app/main.go
@@ -3,16 +3,23 @@ package main
 import (
 	"fmt"
 
+	"github.com/spro80/apiGolang/app/application/orderUseCase"
 	"github.com/spro80/apiGolang/app/infraestructure/controllers/order"
 	"github.com/spro80/apiGolang/app/interfaces/web"
 )
 
+//"github.com/spro80/apiGolang/app/infraestructure/mongo/mongo_connection"
+
 func main() {
 	fmt.Println("Init in main")
 
+	// Database connection
+	//connectionMongo := mongo_connection.GetConnection()
+	//fmt.Println(connectionMongo)
 	//Controller
-	orderController := order.NewOrderController()
 
+	orderUseCase := orderUseCase.NewOrderUseCase()
+	orderController := order.NewOrderController(orderUseCase)
 	//WebServer
 	var port string = "8080"
 	web.NewWebServer()
