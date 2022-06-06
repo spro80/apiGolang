c842ceb855b6f1bc0c64e4cd2b4f103d1dcfae1a
David Diaz <daviddiaz@M-C02FH1TDMD6P.local>
2022-06-06 16:56:14 -0400

    add libraries

--- app/infraestructure/controllers/userControllerGetById/userControllerGetById.go
+++ app/infraestructure/controllers/userControllerGetById/userControllerGetById.go
@@ -6,7 +6,7 @@ import (
 	"github.com/spro80/apiGolang/app/infraestructure/services"
 )
 
-type UserControllerGetByIdInterface interface {
+type UserControlerGetByIdInterface interface {
 	Process(userId string) (services.UserServiceGetByIdResponse, error)
 }
 
@@ -21,13 +21,13 @@ func NewUserControllerGetBydId(service services.UserServiceGetByIdInterface) *Us
 }
 
 func (t *UserControllerGetByIdHandler) Process(id string) (services.UserServiceGetByIdResponse, error) {
-	fmt.Println("[controller][apiGetUsersByIdController] Calling service GetUserById")
+	fmt.Println("[controller][apiGetUsersByIdController] Calling service user_get_by_id")
 	response, err := t.service.ProcessServicesGetUsersById(id)
 	if err != nil {
-		fmt.Println("Error in call Process Services Get Users By Id")
+		fmt.Println("Error in call to Services user_get_by_id")
 		fmt.Println(err.Error())
 		return response, err
 	}
-	fmt.Println("[controller][apiGetUsersByIdController] Service GetUserById was called successfully")
+	fmt.Println("[controller][apiGetUsersByIdController] Service user_get_by_id was called successfully")
 	return response, nil
 }
